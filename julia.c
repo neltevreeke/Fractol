d5ab582f004915492c1addb2cbcf6c3b7c7a75ac
Nelte Vreeke <nvreeke@f0r5s14.codam.nl>
2019-04-02 15:48:47 +0200

    added mouse functionality for julia

--- julia.c
+++ julia.c
@@ -6,42 +6,48 @@
 /*   By: nvreeke <nvreeke@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/29 14:01:12 by nvreeke        #+#    #+#                */
-/*   Updated: 2019/04/01 12:44:36 by nvreeke       ########   odam.nl         */
+/*   Updated: 2019/04/01 13:34:21 by nvreeke       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void        *draw_julia(void *data)
+void		calc_julia(t_mlx *mlx, t_point z, int x, int n)
+{
+	double x_new;
+
+	while (x < WIDTH)
+	{
+		z.x = mlx->position.x + ((x - WIDTH * .5) * 4 / WIDTH) * mlx->zoom;
+		z.y = mlx->position.y + ((mlx->cur_y - HEIGHT * .5) * 4 / WIDTH) * mlx->zoom;
+		n = 0;
+		while (n < mlx->max_it)
+		{
+			x_new = z.x * z.x - z.y * z.y - 0.835 + mlx->c.x / WIDTH;
+			z.y = 2 * z.x * z.y + mlx->c.y / HEIGHT;
+			z.x = x_new;
+			if (fabsl(z.x + z.y) > 6)
+				break ;
+			n++;
+		}
+		if (n < mlx->max_it)
+			put_pixel_to_img(mlx, x, mlx->cur_y, c_pallette(n, z, mlx));
+		x++;
+	}
+}
+
+void     	   *draw_julia(void *data)
 {
     int x;
 	t_point z;
 	t_mlx *mlx;
-	double x_new;
 	int n;
 
 	mlx = (t_mlx*)data;
 	while (mlx->cur_y < mlx->max_y)
 	{
 		x = 0;
-		while (x < WIDTH)
-		{
-			z.x = mlx->position.x + ((x - WIDTH * .5) * 4 / WIDTH) * mlx->zoom;
-			z.y = mlx->position.y + ((mlx->cur_y - HEIGHT * .5) * 4 / WIDTH) * mlx->zoom;
-			n = 0;
-			while (n < mlx->max_it)
-			{
-				x_new = z.x * z.x - z.y * z.y - 0.835 + mlx->c.x / WIDTH;
-				z.y = 2 * z.x * z.y + mlx->c.y / HEIGHT;
-				z.x = x_new;
-				if (fabsl(z.x + z.y) > 6)
-					break ;
-				n++;
-			}
-			if (n < mlx->max_it)
-				put_pixel_to_img(mlx, x, mlx->cur_y, c_pallette(n, z, mlx));
-			x++;
-		}
+		calc_julia(mlx, z, x, n);
 		mlx->cur_y++;
 	}
     return (data);
--- mandelbrot.c
+++ mandelbrot.c
@@ -6,43 +6,49 @@
 /*   By: nvreeke <nvreeke@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/03/22 17:04:03 by nvreeke        #+#    #+#                */
-/*   Updated: 2019/03/29 14:18:19 by nvreeke       ########   odam.nl         */
+/*   Updated: 2019/04/01 13:35:51 by nvreeke       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void		calc_mandelbrot(t_mlx *mlx, t_point z, int x, int n)
+{
+	double x_new;
+
+	while (x < WIDTH)
+	{
+		mlx->c.x = mlx->position.x + ((x - WIDTH * .5) * 4 / WIDTH) * mlx->zoom;
+		mlx->c.y = mlx->position.y + ((mlx->cur_y - HEIGHT * .5) * 4 / WIDTH) * mlx->zoom;
+		z = mlx->c;
+		n = 0;
+		while (n < mlx->max_it)
+		{
+			x_new = z.x * z.x - z.y * z.y + mlx->c.x;
+			z.y = 2 * z.x * z.y + mlx->c.y;
+			z.x = x_new;
+			if (fabsl(z.x + z.y) > 6)
+				break ;
+			n++;
+		}
+		if (n < mlx->max_it)
+			put_pixel_to_img(mlx, x, mlx->cur_y, c_pallette(n, z, mlx));
+		x++;
+	}
+}
 
 void		*draw_mandelbrot(void *data)
 {
 	int x;
 	t_point z;
 	t_mlx *mlx;
-	double x_new;
 	int n;
 
 	mlx = (t_mlx*)data;
 	while (mlx->cur_y < mlx->max_y)
 	{
 		x = 0;
-		while (x < WIDTH)
-		{
-			mlx->c.x = mlx->position.x + ((x - WIDTH * .5) * 4 / WIDTH) * mlx->zoom;
-			mlx->c.y = mlx->position.y + ((mlx->cur_y - HEIGHT * .5) * 4 / WIDTH) * mlx->zoom;
-			z = mlx->c;
-			n = 0;
-			while (n < mlx->max_it)
-			{
-				x_new = z.x * z.x - z.y * z.y + mlx->c.x;
-				z.y = 2 * z.x * z.y + mlx->c.y;
-				z.x = x_new;
-				if (fabsl(z.x + z.y) > 6)
-					break ;
-				n++;
-			}
-			if (n < mlx->max_it)
-				put_pixel_to_img(mlx, x, mlx->cur_y, c_pallette(n, z, mlx));
-			x++;
-		}
+		calc_mandelbrot(mlx, z, x, n);
 		mlx->cur_y++;
 	}
 	return (data);
